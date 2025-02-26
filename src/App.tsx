26e92eeb358746dc4522e184145b231aa8c83d7a
Yasser Cherfaoui <yasser.cherfaoui@gmail.com>
2025-02-26 10:21:34 +0100

    React Query Added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { BrowserRouter } from "react-router-dom";
 import AppRouter from "@/app/routes";
-import { Provider } from "react-redux";
 import { store } from "@/app/store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
 
 export default function App() {
+  const queryClient = new QueryClient();
   return (
     <BrowserRouter>
       <Provider store={store}>
-        <AppRouter />
+        <QueryClientProvider client={queryClient}>
+          <AppRouter />
+        </QueryClientProvider>
       </Provider>
     </BrowserRouter>
   );
