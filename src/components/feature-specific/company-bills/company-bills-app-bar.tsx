daf5f8c42e6866fa5aaa57fe7bb81440b52f0004
Yasser Cherfaoui <yasser.cherfaoui@gmail.com>
2025-02-27 13:44:29 +0100

    using AppBarBackButton in company-bills-app-bar.

--- src/components/feature-specific/company-bills/company-bills-app-bar.tsx
+++ src/components/feature-specific/company-bills/company-bills-app-bar.tsx
@@ -1,22 +1,15 @@
 import { RootState } from "@/app/store";
-import { Button } from "@/components/ui/button";
-import { ArrowLeft } from "lucide-react";
+import AppBarBackButton from "@/components/common/app-bar-back-button";
 import { useSelector } from "react-redux";
-import { useLocation, useNavigate } from "react-router-dom";
 
 export default function () {
   const company = useSelector((state: RootState) => state.company.company);
   if (!company) return;
-  const { pathname } = useLocation();
-  const navigate = useNavigate();
-  const lastLocation = pathname.substring(0, pathname.lastIndexOf("/"));
+
   return (
     <div className="flex justify-between">
       <div className="flex gap-2 items-center text-xl">
-        <Button onClick={()=> navigate(lastLocation)}>
-          <ArrowLeft />
-          Back to Menu
-        </Button>
+       <AppBarBackButton destination="Menu" />
         {company.company_name} &gt; Bills
       </div>
 
