d62ee131da9cec41e9a3f6f2d10a6ee6371b45f9
Yasser Cherfaoui <yasser.cherfaoui@gmail.com>
2025-02-27 13:43:00 +0100

    Franchise Main Activity added.

---
+++ src/components/feature-specific/franchise-dashboard/franchise-menu.tsx
@@ -0,0 +1,40 @@
+import { RootState } from "@/app/store";
+import WideButton from "@/components/common/wide-button";
+import { ReceiptText, ShoppingCart, Warehouse } from "lucide-react";
+import { useSelector } from "react-redux";
+import FranchiseTile from "./franchise-tile";
+
+export default function () {
+  const franchise = useSelector(
+    (state: RootState) => state.franchise.franchise
+  );
+  if (!franchise) return;
+  return (
+    <div className="flex flex-col gap-4 items-center justify-center">
+      <FranchiseTile franchise={franchise} />
+      <div className="flex flex-wrap justify-center gap-4 self-center">
+        {quickMenu.map((item, index) => (
+          <WideButton key={index} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const quickMenu = [
+  {
+    label: "Sales",
+    icon: ShoppingCart,
+    href: "sales",
+  },
+  {
+    label: "Inventory",
+    icon: Warehouse,
+    href: "inventory",
+  },
+  {
+    label: "Bills",
+    icon: ReceiptText,
+    href: "bills",
+  },
+];
---
+++ src/pages/franchise/dashboard/franchise-menu-page.tsx
@@ -0,0 +1,7 @@
+import FranchiseMenu from "@/components/feature-specific/franchise-dashboard/franchise-menu";
+
+export default function () {
+    return <div className="flex flex-col h-screen items-center justify-center">
+        <FranchiseMenu />
+    </div>
+}
