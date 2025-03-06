0f4ae0017871a17472f6b77f7af0e85e615b14ab
Yasser Cherfaoui <yasser.cherfaoui@gmail.com>
2025-03-06 05:37:36 +0100

    modifying the baseUrl constants to submit to the standards of vite envs

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 
 // export const baseUrl = 'https://myerp-production.up.railway.app';
-export const baseUrl = process.env.REACT_APP_BASE_URL || 'http://127.0.0.1:8080'
+export const baseUrl = import.meta.env.VITE_API_URL || 'http://127.0.0.1:8080'
